6d53760f47a7b984ab74a8372ab5d1080a7201c6
Travis Czechorski <tjczec01@gmail.com>
2023-11-16 10:20:10 +0100

    Update csg.py

--- chemsys/csg.py
+++ chemsys/csg.py
@@ -29,7 +29,8 @@ __all__ = ["gui", "symbolgen", "kJtoJ", "create_pdf"]
 warnings.filterwarnings("ignore")  # ,category=matplotlib.cbook.mplDeprecation
 plt.rcParams['text.usetex'] = True
 plt.rcParams['axes.grid'] = False
-plt.rcParams['text.latex.preamble'] = [r'\usepackage{mathtools}', r'\usepackage{bm}']
+plt.rcParams['text.latex.preamble'] = r"\usepackage{bm} \usepackage{amsmath}"
+#plt.rcParams['text.latex.preamble'] = [r'\usepackage{mathtools}', r'\usepackage{bm}']
 # matplotlib.rc_params(fail_on_error=False)
 # plt.rcdefaults()
 print(mpl.get_backend())
